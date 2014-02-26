b3e175ba50990e2ab08acb014bc814280c1e102d
Ed Chang <t.edward.chang@gmail.com>
2014-02-26 01:26:01 -0500

    Heavy edits on header/nav bar, home page to make more streamlined.

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,8 @@ module Tastemaker
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+		
+		# MAKING SURE THAT main.css FROM APP FOLDER IS MOVED INTO PUBLIC FOR USAGE
+		config.assets.precompile << "main.css"
   end
 end
